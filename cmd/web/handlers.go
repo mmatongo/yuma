3fab2c7fd755f244bb78d74cde9b7146f4c50121
Daniel M. Matongo <mmatongo_@hotmail.com>
2024-02-02 23:02:40 +0000

    update handlers

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -5,13 +5,21 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
+	"sort"
 	"strings"
+	"time"
 
 	"github.com/gomarkdown/markdown"
 )
 
 func (a *app) handleBlogPost(w http.ResponseWriter, r *http.Request) {
-	path := strings.TrimPrefix(r.URL.Path, "/")
+	path := strings.TrimPrefix(r.URL.Path, "/_posts/")
+	if r.URL.Path == "/" {
+		a.handleIndex(w, r)
+		return
+	}
+
+	a.infoLog.Printf("Resource requested: %s\n", path)
 
 	mapMutex.RLock()
 	filePath, ok := urlToFileMap[path]
@@ -38,12 +46,48 @@ func (a *app) handleBlogPost(w http.ResponseWriter, r *http.Request) {
 	title := strings.ToUpper(strings.Replace(dashTitle, "-", " ", -1))
 
 	err = tmpl.Execute(w, map[string]interface{}{
-		"Title":   string(title),
-		"Content": template.HTML(output),
+		"Title":      string(title),
+		"Content":    template.HTML(output),
+		"Name":       a.config["name"],
+		"Email":      a.config["email"],
+		"Time":       time.Now().UTC().Format("15:04"),
+		"StatusText": a.config["status"],
+
 		// todo: add date and author to template
 		// add some front matter to the markdown files
 	})
 	if err != nil {
 		a.errorLog.Printf("Template error: %v\n", err)
+	}
+}
+
+func (a *app) handleIndex(w http.ResponseWriter, r *http.Request) {
+	posts := []blogPost{}
+
+	mapMutex.RLock()
+	for url, filePath := range urlToFileMap {
+		title := strings.Replace(strings.TrimPrefix(filePath, "_posts/"), "-", " ", -1)
+		// though golang.org/x/text/case can be used, it's not worth the dependency
+		title = strings.Title(strings.TrimSuffix(title, ".md"))
+		posts = append(posts, blogPost{
+			Title: title,
+			URL:   url,
+		})
+	}
+	mapMutex.RUnlock()
+
+	sort.Slice(posts, func(i, j int) bool {
+		return posts[i].Title < posts[j].Title
+	})
+
+	err := indexTmpl.ExecuteTemplate(w, "index", map[string]interface{}{
+		"Posts":      posts,
+		"Name":       a.config["name"],
+		"Email":      a.config["email"],
+		"Time":       time.Now().UTC().Format("15:04"),
+		"StatusText": a.config["status"],
+	})
+	if err != nil {
+		a.errorLog.Printf("Template execution error: %v\n", err)
 	}
 }
