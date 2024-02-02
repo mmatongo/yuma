bc3beaca39307c1e0cf144525ec4b7e07db6c1fa
Daniel M. Matongo <mmatongo_@hotmail.com>
2024-02-02 22:59:21 +0000

    update main

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -14,22 +14,39 @@ var (
 	urlToFileMap  = make(map[string]string)
 	templateFiles = []string{
 		"./ui/templates/base.layout.gohtml",
+		"./ui/templates/header.partial.gohtml",
+		"./ui/templates/nav.partial.gohtml",
 		"./ui/templates/footer.partial.gohtml",
 	}
-	tmpl     = template.Must(template.ParseFiles(templateFiles...))
-	mapMutex = &sync.RWMutex{}
+
+	indexFiles = []string{
+		"./ui/templates/index.page.gohtml",
+		"./ui/templates/header.partial.gohtml",
+		"./ui/templates/nav.partial.gohtml",
+		"./ui/templates/footer.partial.gohtml",
+	}
+	tmpl      = template.Must(template.ParseFiles(templateFiles...))
+	indexTmpl = template.Must(template.ParseFiles(indexFiles...))
+	mapMutex  = &sync.RWMutex{}
 )
 
 type app struct {
 	errorLog *log.Logger
 	infoLog  *log.Logger
 	watcher  *fsnotify.Watcher
+	config   map[string]interface{}
+}
+
+type blogPost struct {
+	Title string
+	URL   string
 }
 
 func main() {
 	a := &app{
 		errorLog: log.New(os.Stderr, "[ERROR]\t", log.Ldate|log.Ltime|log.Lshortfile),
 		infoLog:  log.New(os.Stdout, "[INFO]\t", log.Ldate|log.Ltime),
+		config:   make(map[string]interface{}),
 	}
 
 	a.initURLToFileMap()
@@ -37,8 +54,13 @@ func main() {
 
 	mux := http.NewServeMux()
 	mux.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./ui/static"))))
-	mux.HandleFunc("/", a.handleBlogPost)
+	mux.HandleFunc("/_posts/", a.handleBlogPost)
+	mux.HandleFunc("/", a.handleIndex)
 
+	a.initConfig()
+	if a.config != nil {
+		a.infoLog.Printf("Config file was successfully loaded\n")
+	}
 	port := os.Getenv("PORT")
 
 	srv := &http.Server{
